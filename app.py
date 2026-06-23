08606436ae4e325905451d244c4eaf0ae54c3db7
sathishmk24 <mksathish50@gmail.com>
2026-06-23 19:38:12 +0530

    Updated templates and navbar

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
+from werkzeug.security import generate_password_hash, check_password_hash
 import sqlite3
 import os
 import PyPDF2
@@ -7,7 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
 app = Flask(__name__)
-
+app.secret_key = "placementportal123"
 UPLOAD_FOLDER = "uploads"
 app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
@@ -226,6 +227,84 @@ def recommend():
         recommendations=recommendations,
         score=resume_score
     )
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/register-user", methods=["POST"])
+def register_user():
+    name = request.form["name"]
+    email = request.form["email"]
+
+    password = generate_password_hash(
+    request.form["password"]
+  )
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO users(name,email,password) VALUES(?,?,?)",
+        (name, email, password)
+  )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/login")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/login-user", methods=["POST"])
+def login_user():
+
+
+    email = request.form["email"]
+    password = request.form["password"]
+
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT * FROM users WHERE email=?",
+        (email,)
+  )
+
+    user = cursor.fetchone()
+
+    conn.close()
+
+    if user and check_password_hash(user[3], password):
+        session["user"] = user[1]
+        return redirect("/dashboard")
+
+    return "Invalid Email or Password"
+
+
+
+@app.route("/dashboard")
+def dashboard():
+
+    if "user" not in session:
+        return redirect("/login")
+
+    return render_template(
+        "dashboard.html",
+        user=session["user"]
+  )
+
+
+@app.route("/logout")
+def logout():
+
+
+    session.clear()
+
+    return redirect("/login")
+
 
 
 if __name__ == "__main__":
